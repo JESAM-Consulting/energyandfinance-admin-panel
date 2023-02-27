0cb08f38c54182e41fe6a92686db6fae43a2c996
Kevin <kevin088.rejoice@gmail.com>
2023-02-27 11:56:45 +0530

    Fix issue of Qualifiziert

--- src/_metronic/components/Qualifiziert/Qualifiziert.tsx
+++ src/_metronic/components/Qualifiziert/Qualifiziert.tsx
@@ -384,104 +384,27 @@ export default function Qualifiziert() {
             width: "8%"
         },
         {
-            name: "Postleitzahl",
-            selector: (row: any) => (row?.postalCode ? row?.postalCode : "-"),
-            sortable: true,
+            name: "bist_du_derzeit_berufstätig?",
+            selector: (row: any) => (row?.isEmployed ? row?.isEmployed : "-"),
+            sortable:true
         },
         {
-            name: "Datum",
-            selector: (row: any) => moment(row?.createdAt).format("DD/MM/YYYY"),
-            width: "10%"
+            name: "wie_viel_vertriebserfahrung_hast_du?",
+            selector: (row: any) => (row?.salesExperience ? row?.salesExperience : "-"),
+            sortable:true
         },
         {
-            name: "SMS vorher ",
-            selector: (row: any) => (row?.sms ? (row?.sms === true ? "Ja" : "Nein") : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
+            name: "bitte_wähle_die_auf_dich_zutreffende_antwort_aus:_ich_bin_...",
+            selector: (row: any) => (row?.answer ? row?.answer : "-"),
+            sortable:true
         },
         {
-            name: "Kontaktiert durch",
-            selector: (row: any) => (row?.contactedBy ? row?.contactedBy : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-        {
-            name: "Kontaktiert am",
-            selector: (row: any) => (row?.contactedOn ? `${moment(row?.contactedOn).utc().format("DD/MM/YYYY")}` : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-
-        {
-            name: "erneut kontaktiert",
-            selector: (row: any) => (row?.contactedAgain ? `${moment(row?.contactedAgain).utc().format("DD/MM/YYYY")}` : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-        {
-            name: "letzmalige Kontaktaufnahme",
-            selector: (row: any) => (row?.lastContact ? `${moment(row?.lastContact).utc().format("DD/MM/YYYY")}` : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-
-        {
-            name: "E-Mail bei 3 mal nicht erreicht",
-            selector: (row: any) => (row?.emailFailed ? (row?.emailFailed === true ? "Ja" : "Nein") : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-        },
-        {
-            name: "PV",
-            selector: (row: any) => (row?.pv ? (row?.pv === true ? "Ja" : "Nein") : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-        },
-
-        {
-            name: "Erreicht ",
-            selector: (row: any) => (row?.reached ? (row?.reached === true ? "Ja" : "Nein") : "-"),
-            // selector: (row: any) => row?.createdAt,
-
+            name: "state",
+            selector: (row: any) => (row?.state ? row?.state : "-"),
+            sortable:true
         },
 
 
-        {
-            name: "Termin am",
-            selector: (row: any) => (row?.appointmentDate ? `${moment(row?.appointmentDate).utc().format("DD/MM/YYYY")}` : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-        {
-            name: "Termin Uhrzeit",
-            selector: (row: any) => (row?.appointmentTime ? row?.appointmentTime : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-        {
-            name: "Termin macht",
-            selector: (row: any) => (row?.makeAppointment ? row?.makeAppointment : "-"),
-            // selector: (row: any) => row?.createdAt,
-
-
-        },
-        {
-            name: "Wissenswertes",
-            selector: (row: any) => (row?.usefulInformation ? row?.usefulInformation : "-"),
-            // selector: (row: any) => row?.createdAt,
-        },
-        {
-            name: "nicht geeignet",
-            selector: (row: any) => (row?.nichtGeeignet ? (row?.nichtGeeignet === true ? "yes" : "no") : "-"),
-            // selector: (row: any) => row?.createdAt,
-        },
     ];
     return (
         <>
